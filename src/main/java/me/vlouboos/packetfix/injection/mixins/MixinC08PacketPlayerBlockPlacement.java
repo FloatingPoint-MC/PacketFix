64ca38d4153d47e6b1e0b477cd4b504599e95350
vlouboos <87124020+vlouboos@users.noreply.github.com>
2022-07-21 16:30:50 +0800

    Update MixinC08PacketPlayerBlockPlacement.java

--- src/main/java/me/vlouboos/packetfix/injection/mixins/MixinC08PacketPlayerBlockPlacement.java
+++ src/main/java/me/vlouboos/packetfix/injection/mixins/MixinC08PacketPlayerBlockPlacement.java
@@ -7,6 +7,7 @@ import net.minecraft.util.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import me.vlouboos.packetfix.utils.FixEngine;
 
 import java.io.IOException;
 
@@ -34,9 +35,9 @@ public class MixinC08PacketPlayerBlockPlacement {
         buf.writeBlockPos(this.position);
         buf.writeByte(this.placedBlockDirection);
         buf.writeItemStackToBuffer(this.stack);
-        buf.writeByte((int) (this.facingX));
-        buf.writeByte((int) (this.facingY));
-        buf.writeByte((int) (this.facingZ));
+        buf.writeByte((int) (this.facingX * FixEngine.fixRightClick()));
+        buf.writeByte((int) (this.facingY * FixEngine.fixRightClick()));
+        buf.writeByte((int) (this.facingZ * FixEngine.fixRightClick()));
     }
 
     /**
@@ -49,8 +50,8 @@ public class MixinC08PacketPlayerBlockPlacement {
         this.position = buf.readBlockPos();
         this.placedBlockDirection = buf.readUnsignedByte();
         this.stack = buf.readItemStackFromBuffer();
-        this.facingX = buf.readUnsignedByte();
-        this.facingY = buf.readUnsignedByte();
-        this.facingZ = buf.readUnsignedByte();
+        this.facingX = buf.readUnsignedByte() / FixEngine.fixRightClick();
+        this.facingY = buf.readUnsignedByte() / FixEngine.fixRightClick();
+        this.facingZ = buf.readUnsignedByte() / FixEngine.fixRightClick();
     }
 }
